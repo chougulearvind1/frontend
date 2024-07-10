079edfd82ec0a7f9e8bcbb376dfc32a69b3b11f0
unknown <chougulearvind1@gmail.com>
2024-07-10 08:34:46 -0700

    if tweet has retweet array then it retweet created with Retweeet user but problem is that in render some retweets lost so retweet create at severside  and remove retweets component from  TWeets format

--- src/Component/Home_Component/TweetList.tsx
+++ src/Component/Home_Component/TweetList.tsx
@@ -1,5 +1,5 @@
 
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import Tweets from './Tweets';
 import TweetModal from './TweetModal';
 import Cookies from 'js-cookie';
@@ -10,29 +10,49 @@ interface TweetListProps{
   AllTweet:any
 }
 const TweetList:React.FC<TweetListProps> = ({AllTweet}) => { 
-  
-    const [Retweet, setRetweet] = useState<any>([]);
+     const [TweetData, setTweetData] = useState(AllTweet)
+    const [Retweet, setRetweet] = useState<any[]>([]);
     const [ShowModal, setShowModal] = useState(false);  
+    const [NewTweet, setNewTweet] = useState<any>()
     const userId=Cookies.get('id')   
-    const closeModal = () => {      
+    const closeModal = (Tweet: any) => {  
+        console.log(Tweet,'tweet in tweetlist');   
+        console.log(TweetData,'all tweet1'); 
+        
+        setNewTweet(Tweet)
+        // setTweetData((prevTweetData: any) => [Tweet, ...prevTweetData])
+        console.log(TweetData,'all tweet2'); 
+
         setShowModal(false)        
        }
        const OpenModal = () => {
         setShowModal(true);        
        }      
+      //  const [UpdatedReTweets, setUpdatedReTweets] = useState<any[]>()
       const OnReTweet = async (params: any,Tweet:any) => {        
-        // let RetweetArr:any=[];      
+              
         if(Tweet && Tweet.retweetBy.every((item:any)=>typeof item==='object')){          
-          const updatedRetweets= await Promise.all(Tweet.retweetBy.map(async (item: any,index: any) => {
-            const updatedTweetObj={...Tweet, ReTweetUser:item.UserName}           
+          let updatedRetweets= await Promise.all(Tweet.retweetBy.map(async (item: any,index: any) => {
+            const updatedTweetObj={...Tweet, ReTweetUser:item.UserName}        
             return updatedTweetObj;          
           }) )  
-          if(updatedRetweets){ 
-            setRetweet(updatedRetweets)            
+          if(updatedRetweets.length>0){ 
+          
+             setRetweet((prevTweet: any) =>[...prevTweet,...updatedRetweets].reverse())                     
           }
-          console.log(AllTweet,'all tweets')
+       
               }      
-      }      
+      }   
+      useEffect(() => {
+        console.log(TweetData, 'all tweet effect');
+      }, [TweetData]);
+      // useEffect(() => {
+      //   console.log('Retweets updated:', Retweet);
+      
+        
+      // }, [Retweet, setRetweet])
+      
+      
    return(
     <div>
         <TweetModal show={ShowModal} closeModal={closeModal} > </TweetModal>
@@ -44,13 +64,18 @@ const TweetList:React.FC<TweetListProps> = ({AllTweet}) => {
       </div>
         <ul style={{padding:'0',margin:'0', height:'100vh',overflowY:'auto'}}>        
           {/* If retweet has retweets of array then every tweet indicate name  on tweet  whoes is retweet */}
-          {Retweet && Retweet.map((item:any,index:any)=> 
-            (<li  key={'a'+Date.now()+index} className='mb-2' style={{listStyleType:'none',padding:'0',margin:'0'}}>
-                  <Tweets key={'a'+Date.now()} TweetData={item} userId={userId} OnReTweet={OnReTweet}></Tweets>
+          {/* {Retweet && Retweet.map((item:any,index:any)=> 
+            (<li  key={'b'+index} className='mb-2' style={{listStyleType:'none',padding:'0',margin:'0'}}>
+                  <Tweets  TweetData={item} userId={userId} OnReTweet={OnReTweet}></Tweets>
               </li>)
-          )}       
-        
-        { AllTweet && AllTweet.map(
+          )}        */}
+        {/* {JSON.stringify(TweetData)} */}
+        {NewTweet && (
+          <li  key={NewTweet._id}  className='mb-2' style={{listStyleType:'none',padding:'0',margin:'0'}}>                  
+                  <Tweets key={'a'} TweetData={NewTweet} userId={userId} OnReTweet={OnReTweet}></Tweets>                 
+              </li>
+        )}
+        { TweetData &&  TweetData.map(
           (tweet: any,index: React.Key ) => (            
             <li  key={'a'+index}  className='mb-2' style={{listStyleType:'none',padding:'0',margin:'0'}}>                  
                   <Tweets key={'a'+index} TweetData={tweet} userId={userId} OnReTweet={OnReTweet}></Tweets>                 
